ed7572cc7117147db183697cb18193c365648250
Urbina Tatiana <urbinatatiana077@gmail.com>
2023-02-17 16:14:46 -0300

    ejercicio repaso listas

--- LISTAS/porcionLista.py
+++ LISTAS/porcionLista.py
@@ -1,4 +1,5 @@
 lista_nombres_perros= ["Rocco", "Gretta", "Pancho", "Zehus", "Lupe", "Gringo"]
+#SUBLISTAS 
 print(lista_nombres_perros[2:])#muestra del indice 2 hasta el final
 print(lista_nombres_perros[:3])#muestra del indice 0 hasta el anterior al 3
 print(lista_nombres_perros[-2])#ante ultimo elemento
